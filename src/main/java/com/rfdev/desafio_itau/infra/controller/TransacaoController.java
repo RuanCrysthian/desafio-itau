633458455f85b635298d73c5f2a1cd1ad7926aad
Ruan Ferraz <ruanlima155@gmail.com>
2025-04-06 08:47:38 -0300

    feat:swagger documentation

--- src/main/java/com/rfdev/desafio_itau/infra/controller/TransacaoController.java
+++ src/main/java/com/rfdev/desafio_itau/infra/controller/TransacaoController.java
@@ -10,6 +10,9 @@ import com.rfdev.desafio_itau.application.usecase.transacao.CriarTransacaoInputD
 import com.rfdev.desafio_itau.application.usecase.transacao.CriarTransacaoOutputDto;
 import com.rfdev.desafio_itau.application.usecase.transacao.CriarTransacaoUseCase;
 import com.rfdev.desafio_itau.domain.transacao.TransacaoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,18 +29,32 @@ public class TransacaoController {
     estatisticaTransacoesUseCase = new EstatisticaTransacoesUseCase(transacaoRepository);
   }
 
+  @Operation(description = "Criar uma transação.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Criar uma transação com sucesso"),
+    @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+    @ApiResponse(responseCode = "422", description = "Erro de validação nos dados de entrada"),
+  })
   @PostMapping("/transacao")
   public ResponseEntity<CriarTransacaoOutputDto> criarTransacao(@RequestBody CriarTransacaoInputDto input) {
     CriarTransacaoOutputDto output = criarTransacaoUseCase.executar(input);
     return ResponseEntity.status(HttpStatus.CREATED).body(output);
   }
 
+  @Operation(description = "Limpar todas as transações.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Limpar todas as transações com sucesso")
+  })
   @DeleteMapping("/transacao")
   public ResponseEntity<LimparTransacaoOuputDto> limparTransacao() {
     LimparTransacaoOuputDto output = limparTransacaoUseCase.executar(new LimparTransacaoInputDto());
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(output);
   }
 
+  @Operation(description = "Consultar estatísticas das transações em um espaço de tempo de 60 segundos.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Consulta realizada com sucesso"),
+  })
   @GetMapping("/estatistica")
   public ResponseEntity<EstatisticaTransacoesOutputDto> estatisticaTransacoes() {
     EstatisticaTransacoesOutputDto output = estatisticaTransacoesUseCase.execute(new EstatisticaTransacoesInputDto());
